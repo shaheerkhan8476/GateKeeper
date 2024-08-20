8c831619bbc526018c7f6f121165a5ffe4b840ef
Shaheer Khan <119394707+shaheerkhan8476@users.noreply.github.com>
2024-08-19 22:32:05 -0400

    Profile UI
    Added a filler profile picture for Profile UI

--- User/ProfileView.swift
+++ User/ProfileView.swift
@@ -15,18 +15,32 @@ struct ProfileView: View {
     var body: some View {
         if let userData = userViewModel.userData {
             VStack {
+                ZStack(alignment: .bottomTrailing){
+                            Image(systemName: "heart.circle.fill")
+                                 .resizable()
+                                 .frame(width:100, height: 100)
+                                 .clipShape(Circle())
+                            Image(systemName: "plus.circle")
+                                 .foregroundColor(.white)
+                                 .frame(width: 25, height: 25)
+                                 .background(Color.blue)
+                                 .clipShape(Circle())
+                               
+                                .overlay(Circle().stroke(Color.white, lineWidth: 2))
+                            }
+                .padding()
                 Spacer()
                 HStack{
                     Text("Name: ").bold()
                     Spacer()
                     Text(userData.name ?? "User Data").bold()
-                }
+                }.padding()
                 Divider()
                 HStack {
                     Text("Email: ").bold()
                     Spacer()
                     Text(userData.email ?? "No Email").bold()
-                }
+                }.padding()
                 Spacer()
                 Button(action: {
                     do {
